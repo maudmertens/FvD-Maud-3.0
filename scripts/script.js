c0f99d49707291a91c94a95d3faf8155360aa4a2
maudmertens <maud.mertens.13@hotmail.com>
2025-12-03 17:19:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,23 @@
 // JavaScript Document
 console.log("hi");
+
+// oke dit is voor het kruisje, maar doet t nu niet meer
+var vegaBurgerMenu = document.querySelector("header button");
+vegaBurgerMenu.onclick = vegaBurgerMenuKlik;
+function vegaBurgerMenuKlik() {
+    if
+    (vegaBurgerMenu.ariaExpanded == "false") {
+    vegaBurgerMenu.ariaExpanded = "true"
+    }
+    else {
+    vegaBurgerMenu.ariaExpanded = "false"
+    }
+}
+
+var vegaBurgerMenu = document.querySelector("header button");
+vegaBurgerMenu.onclick = toggleMenu;
+function toggleMenu() {
+    var deNav = document.querySelector("header nav");
+    deNav.classList.toggle("toonMenu");
+}
+
